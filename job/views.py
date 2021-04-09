f20dd040aef131c094e7255a0f1866918aa3dfea
gitAlbertadine <albertadine760@gmail.com>
2021-04-09 19:42:23 +0100

    g

--- job/views.py
+++ job/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
 def job_list(request):
-    pass
+    job_list=Job.objects.all()
+    context={'jobs':job_list}
+    return render(request,'job/job_list.html',context)
 def job_detail(request,id):
     pass
