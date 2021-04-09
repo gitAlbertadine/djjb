53aaf00af537edda427a2b5ae77e4cbc077d7f73
gitAlbertadine <albertadine760@gmail.com>
2021-04-09 21:30:36 +0100

    views

--- job/views.py
+++ job/views.py
@@ -7,4 +7,6 @@ def job_list(request):
     context={'jobs':job_list}
     return render(request,'job/job_list.html',context)
 def job_detail(request,id):
-    pass
+    job_detail=Job.objects.get(id=id)
+    context={'job':job_detail}
+    return render(request,'job/job_detail.html',context)
