b8a4676639d1100be1853d234d317b942271fce8
gitAlbertadine <albertadine760@gmail.com>
2021-04-10 12:27:28 +0100

    class views

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.job_list),
-    path('<int:id>', views.job_detail),
+    path('', views.JobList.as_view(), name='job_list'),
+    path('<int:pk>', views.JobDetail.as_view()),
 
 ]
--- job/views.py
+++ job/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Job
 # Create your views here.
 
-def job_list(request):
-    job_list=Job.objects.all()
-    context={'jobs':job_list}
-    return render(request,'job/job_list.html',context)
-def job_detail(request,id):
-    job_detail=Job.objects.get(id=id)
-    context={'job':job_detail}
-    return render(request,'job/job_detail.html',context)
+#def job_list(request):
+#    job_list=Job.objects.all()
+#    context={'jobs':job_list}
+#    return render(request,'job/job_list.html',context)
+#def job_detail(request,id):\
+#    job_detail=Job.objects.get(id=id)
+#    context={'job':job_detail}
+#    return render(request,'job/job_detail.html',context)
+class JobList(ListView): 
+    model = Job
+class JobDetail(DetailView):
+    model = Job
+class JobCreate():
+    pass
+class JobDelete():
+    pass
+class JobUpdate():
+    pass
